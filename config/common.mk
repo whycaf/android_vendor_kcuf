8f84e3af67d8a3ea540e94432e0fb3b0ad85f4d2
LineageOS <lineageos.org>
2018-08-13 04:16:05 +0000

    add backup tool from lineage

--- config/common.mk
+++ config/common.mk
@@ -13,3 +13,17 @@ include vendor/kcuf/config/props.mk
 
 # Packages
 include vendor/kcuf/config/packages.mk
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/kcuf/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/kcuf/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/kcuf/prebuilt/common/bin/50-kcuf.sh:system/addon.d/50-kcuf.sh \
+    vendor/kcuf/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/kcuf/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/kcuf/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/kcuf/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
